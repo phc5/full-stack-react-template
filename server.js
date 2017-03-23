2003b746ea2e3fab497488f524102f088b2b6c1c
PaulChong <paulhyunchong@gmail.com>
2017-03-23 10:09:19 -0700

    edit index.html and change route for '/'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
 
